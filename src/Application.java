d55b7a780979fb490254989c3e6f509395b4d6b9
AG-2 <47139184+AG-2@users.noreply.github.com>
2019-02-24 14:34:30 +0100

    Add files via upload

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,19 @@
 import Airport.Airport;
+import Airport.Location;
+import Airport.Runway;
+import Airport.Airplane.Aircraft;
+import Airport.Tower;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Airport airport = new Airport("BER");
-
+		Runway runway;
+		Location location;
+		Aircraft aircraft;
+		
+		airport.buildLocations();
+		
 	}
 
 }
